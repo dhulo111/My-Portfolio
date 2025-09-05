ea78dbb3050de582aa4af9c689056d6efa43909d
dhulo_11 <dhruvanpatel8866@gmail.com>
2025-09-05 16:40:05 +0530

    some changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react";
-import {Cn} from '@/lib/utils.js';
-import { X,Menu } from "lucide-react";
-const navitem=[
-  {name:"home", href: "#hero"},
-  {name:"About",href:"#about"},
-  {name:"skill",href:"#skill"},
-  {name:"project",href:"#project"},
-  {name:"contact",href:"#contact"},
+import { Cn } from '@/lib/utils.js';
+import { X, Menu } from "lucide-react";
+const navitem = [
+  { name: "home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "skill", href: "#skill" },
+  { name: "project", href: "#project" },
+  { name: "contact", href: "#contact" },
 ]
-function Navbar(){
-  const [isScrolled,setIsScroll]=useState(true);
-  const [isMenuOpen,setIsMenuOpen]=useState(false);
-  useEffect(()=>{
-   const handleScroll=()=>{
-    setIsScroll(window.screenY>10);
-   };
-    window.addEventListener("scroll",handleScroll);
-    return ()=>window.removeEventListener("scroll",handleScroll);
+function Navbar() {
+  const [isScrolled, setIsScroll] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScroll(window.screenY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
 
-  },[])
+  }, [])
   return (
     <nav className={Cn("fixed w-full z-40 transition-all duration-300",
-      isScrolled ? "py-5":"py-3 bg-background/80 backdrop-blur-md shadow-xs")}>
+      isScrolled ? "py-5" : "py-3 bg-background/80 backdrop-blur-md shadow-xs")}>
       <div className="container flex item-center justify-between">
         <a className="text-xl font-bold text-primary flex items-center" href="#hero">
           <span className="relative z-10">
@@ -31,36 +31,36 @@ function Navbar(){
 
         {/* desktop nav */}
         <div className="hidden md:flex space-x-8">
-         {navitem.map((item,key)=>(
-           <a key={key} href={item.href} className="text-foreground /80 hover:text-primary transition-colors duration-300">{item.name}</a>
-         ))}
+          {navitem.map((item, key) => (
+            <a key={key} href={item.href} className="text-foreground /80 hover:text-primary transition-colors duration-300">{item.name}</a>
+          ))}
         </div>
         {/* mobile nav */}
 
-         <button onClick={()=>setIsMenuOpen((prev)=>!prev)}
-          className="md:hidden p-2 text-foreground z-50"
+        <button onClick={() => setIsMenuOpen((prev) => !prev)}
+          className="md:hidden p-2 max-sm:pr-14 text-foreground z-50"
           aria-label={isMenuOpen ? "close menu" : "open menu"}
-         >{isMenuOpen ? <X size={24}/> : <Menu size={24}/>}</button>
+        >{isMenuOpen ? <X size={24} /> : <Menu size={24} />}</button>
 
-        <div className={Cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col item-center justify-center",
+        <div className={Cn("fixed inset-0 z-40 bg-background/60 h-[269px] flex flex-col ",
           "transition-all duration-300 md:hidden",
           isMenuOpen ? "opacity-100 pointer-events-auto"
-                     : "opacity-0 pointer-events-none"
+            : "opacity-0 pointer-events-none"
         )}>
-         <div className="flex flex-col space-y-8 text-xl">
-          {navitem.map((item,key)=>(
-            <a 
-              key={key} 
-              href={item.href}
-              className="text-foreground /80 hover:text-primary transition-colors duration-300"
-              onClick={()=>setIsMenuOpen(false)}
+          <div className="flex flex-col   space-y-8 text-xl">
+            {navitem.map((item, key) => (
+              <a
+                key={key}
+                href={item.href}
+                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
               >{item.name}</a>
-          ))}
-         </div>
+            ))}
+          </div>
         </div>
       </div>
     </nav>
-  ) 
+  )
 }
 
 export default Navbar;
